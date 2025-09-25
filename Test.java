dfbd8918c08e951f31d45f366e4ca05a368cbcad
Syeril <syerilazalea20@gmail.com>
2025-09-25 09:45:04 +0700

    nambah 1

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Hi, ini baris kode kedua");
         System.out.println("Hi, ini baris kode kode");
+        System.out.println("Hi, ini baris kok kode kode kode");
     }
 }
